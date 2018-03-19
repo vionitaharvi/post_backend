4e087b6b7aa7b9016f69f3bb41b460b517d8857f
vionitaharvi <vionita.harvi16@gmail.com>
2018-03-19 15:00:17 +0700

    tes komit kedua

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+	<title></title>
+</head>
+<body>
+<form action="list.php" method="get">
+<br><br><br>
+
+<table width="350px" align="center" border="1">
+<tr><td> <br><center>Pencarian dengan nama jurusan<br><br>
+<table align="center" height="100px">
+	<tr><td><input type="text" name="cari"></td></tr>
+	<tr><td><input type="submit" value="cari"></td></tr>
+
+</table>
+<br><br><br></td></tr>
+</table>
+</form>
+</body>
+</html>
+
